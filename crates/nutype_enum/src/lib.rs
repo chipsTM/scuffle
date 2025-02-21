7fdf115a296b380402052275f7b99033a618ccba
Lennart Kloock <lennart@scuffle.cloud>
2025-02-21 19:21:08 +0100

    Revert "fix(nutype-enum): allow missing docs for generated code"
    This reverts commit 31ebc027244e9571ac0f01ec6f017d10b023da9f.

--- crates/nutype_enum/src/lib.rs
+++ crates/nutype_enum/src/lib.rs
@@ -75,7 +75,6 @@ macro_rules! nutype_enum {
             $(
                 $(#[$variant_attr])*
                 #[allow(non_upper_case_globals)]
-                #[allow(missing_docs)]
                 pub const $variant: Self = Self($value);
             )*
         }
