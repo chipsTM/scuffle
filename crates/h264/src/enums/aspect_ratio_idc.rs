383d11663a1c7549c3e2230056a3c65a9c24d42b
philipch07 <philipch07@gmail.com>
2025-02-23 18:21:36 -0500

    add examples

--- crates/h264/src/enums/aspect_ratio_idc.rs
+++ crates/h264/src/enums/aspect_ratio_idc.rs
@@ -19,51 +19,93 @@ nutype_enum! {
         Unspecified = 0,
 
         /// 1: 1:1 (square)
+        /// ## Examples
+        /// - 7680 x 4320 16:9 w/o horizontal overscan
+        /// - 3840 x 2160 16:9 w/o horizontal overscan
+        /// - 1280 x 720 16:9 w/o horizontal overscan
+        /// - 1920 x 1080 16:9 w/o horizontal overscan (cropped from 1920x1088)
+        /// - 640 x 480 4:3 w/o horizontal overscan
         Square = 1,
 
         /// 2: 12:11
+        /// ## Examples
+        /// - 720 x 576 4:3 with horizontal overscan
+        /// - 352 x 288 4:3 w/o horizontal overscan
         Aspect12_11 = 2,
 
         /// 3: 10:11
+        /// ## Examples
+        /// - 720 x 480 4:3 with horizontal overscan
+        /// - 352 x 240 4:3 w/o horizontal overscan
         Aspect10_11 = 3,
 
         /// 4: 16:11
+        /// ## Examples
+        /// - 720 x 576 16:9 with horizontal overscan
+        /// - 528 x 576 4:3 w/o horizontal overscan
         Aspect16_11 = 4,
 
         /// 5: 40:33
+        /// ## Examples
+        /// - 720 x 480 16:9 with horizontal overscan
+        /// - 528 x 480 4:3 w/o horizontal overscan
         Aspect40_33 = 5,
 
         /// 6: 24:11
+        /// ## Examples
+        /// - 352 x 576 4:3 w/o horizontal overscan
+        /// - 480 x 576 16:9 with horizontal overscan
         Aspect24_11 = 6,
 
         /// 7: 20:11
+        /// ## Examples
+        /// - 352 x 480 4:3 w/o horizontal overscan
+        /// - 480 x 480 16:9 with horizontal overscan
         Aspect20_11 = 7,
 
         /// 8: 32:11
+        /// ## Example
+        /// - 352 x 576 16:9 w/o horizontal overscan
         Aspect32_11 = 8,
 
         /// 9: 80:33
+        /// ## Example
+        /// - 352 x 480 16:9 w/o horizontal overscan
         Aspect80_33 = 9,
 
         /// 10: 18:11
+        /// ## Example
+        /// - 480 x 576 16:9 with horizontal overscan
         Aspect18_11 = 10,
 
         /// 11: 15:11
+        /// ## Example
+        /// - 480 x 480 4:3 with horizontal overscan
         Aspect15_11 = 11,
 
         /// 12: 64:33
+        /// ## Example
+        /// - 528 x 576 16:9 w/o horizontal overscan
         Aspect64_33 = 12,
 
         /// 13: 160:99
+        /// ## Example
+        /// - 528 x 480 16:9 w/o horizontal overscan
         Aspect160_99 = 13,
 
         /// 14: 4:3
+        /// ## Example
+        /// - 1440 x 1080 16:9 w/o horizontal overscan
         Aspect4_3 = 14,
 
         /// 15: 3:2
+        /// ## Example
+        /// - 1280 x 1080 16:9 w/o horizontal overscan
         Aspect3_2 = 15,
 
         /// 16: 2:1
+        /// ## Example
+        /// - 960 x 1080 16:9 w/o horizontal overscan
         Aspect2_1 = 16,
 
         /// 17..=254: Reserved (should be ignored)
