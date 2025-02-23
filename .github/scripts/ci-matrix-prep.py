c6474c664bb3fe9ab8c7660f001787d4209fbacb
Troy Benson <troybensonsa@gmail.com>
2025-02-23 04:02:15 +0000

    better design

--- .github/scripts/ci-matrix-prep.py
+++ .github/scripts/ci-matrix-prep.py
@@ -12,6 +12,7 @@ GITHUB_DEFAULT_RUNNER = "ubuntu-24.04"
 LINUX_X86_64 = "ubicloud-standard-8"
 LINUX_ARM64 = "ubicloud-standard-8-arm"
 
+
 def is_brawl(mode: Optional[str] = None) -> bool:
     if mode is None:
         mode = ""
@@ -35,8 +36,10 @@ def pr_number() -> Optional[int]:
 
     return None
 
+
 # The output should be in the form
 # matrix=<json>
+
 
 @dataclass
 class RustSetup:
@@ -46,66 +49,68 @@ class RustSetup:
     tools: str = ""
     cache_backend: str = "ubicloud"
 
+
 @dataclass
 class DocsMatrix:
-    os: str
     artifact_name: Optional[str]
     deploy_docs: bool
-    job_name: str
     pr_number: Optional[int]
-    rust: RustSetup
+
 
 @dataclass
 class ClippyMatrix:
-    os: str
     powerset: bool
-    job_name: str
-    rust: RustSetup
+
 
 @dataclass
 class TestMatrix:
-    os: str
-    job_name: str
     pr_number: Optional[int]
     commit_sha: str
-    rust: RustSetup
+
 
 @dataclass
 class GrindMatrix:
-    os: str
-    job_name: str
     env: str
-    rust: RustSetup
+
 
 @dataclass
 class FmtMatrix:
-    os: str
-    job_name: str
-    rust: RustSetup
+    pass
+
 
 @dataclass
 class HakariMatrix:
-    os: str
-    job_name: str
-    rust: RustSetup
+    pass
+
 
 @dataclass
 class Job:
+    runner: str
     job_name: str
-    inputs: GrindMatrix | DocsMatrix | ClippyMatrix | TestMatrix | FmtMatrix | HakariMatrix
+    rust: RustSetup
+    ffmpeg: bool
+    inputs: (
+        GrindMatrix | DocsMatrix | ClippyMatrix | TestMatrix | FmtMatrix | HakariMatrix
+    )
+    job: str
 
-def create_docs_jobs() -> list[DocsMatrix]:
-    jobs: list[DocsMatrix] = []
+
+def create_docs_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     jobs.append(
-        DocsMatrix(
-            os=LINUX_X86_64,
-            artifact_name="docs",
-            # if its brawl merge, we don't want to deploy docs
-            # since that will be deployed after the merge is successful
-            deploy_docs=not is_brawl("merge"),
-            job_name=f"Docs (Linux x86_64)",
-            pr_number=pr_number(),
+        Job(
+            runner=LINUX_X86_64,
+            job_name="Docs (Linux x86_64)",
+            job="docs",
+            ffmpeg=True,
+            inputs=DocsMatrix(
+                artifact_name="docs",
+                # if its brawl merge, we don't want to deploy docs
+                # since that will be deployed after the merge is successful
+                deploy_docs=not is_brawl("merge"),
+                pr_number=pr_number(),
+            ),
             rust=RustSetup(
                 toolchain="nightly",
                 components="rust-docs",
@@ -118,12 +123,16 @@ def create_docs_jobs() -> list[DocsMatrix]:
 
     if is_brawl():
         jobs.append(
-            DocsMatrix(
-                os=LINUX_ARM64,
-                artifact_name=None,
-                deploy_docs=False,
+            Job(
+                runner=LINUX_ARM64,
                 job_name=f"Docs (Linux arm64)",
-                pr_number=pr_number(),
+                job="docs",
+                ffmpeg=True,
+                inputs=DocsMatrix(
+                    artifact_name=None,
+                    deploy_docs=False,
+                    pr_number=pr_number(),
+                ),
                 rust=RustSetup(
                     toolchain="nightly",
                     components="rust-docs",
@@ -137,14 +146,18 @@ def create_docs_jobs() -> list[DocsMatrix]:
     return jobs
 
 
-def create_clippy_jobs() -> list[ClippyMatrix]:
-    jobs: list[ClippyMatrix] = []
+def create_clippy_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     jobs.append(
-        ClippyMatrix(
-            os=LINUX_X86_64,
-            powerset=is_brawl(),
+        Job(
+            runner=LINUX_X86_64,
             job_name=f"Clippy (Linux x86_64)",
+            job="clippy",
+            ffmpeg=True,
+            inputs=ClippyMatrix(
+                powerset=is_brawl(),
+            ),
             rust=RustSetup(
                 toolchain="nightly",
                 components="rust-clippy",
@@ -157,10 +170,14 @@ def create_clippy_jobs() -> list[ClippyMatrix]:
 
     if is_brawl():
         jobs.append(
-            ClippyMatrix(
-                os=LINUX_ARM64,
-                powerset=True,
+            Job(
+                runner=LINUX_ARM64,
                 job_name=f"Clippy (Linux arm64)",
+                job="clippy",
+                ffmpeg=True,
+                inputs=ClippyMatrix(
+                    powerset=True,
+                ),
                 rust=RustSetup(
                     toolchain="nightly",
                     components="rust-clippy",
@@ -174,8 +191,8 @@ def create_clippy_jobs() -> list[ClippyMatrix]:
     return jobs
 
 
-def create_test_jobs() -> list[TestMatrix]:
-    jobs: list[TestMatrix] = []
+def create_test_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     commit_sha = os.environ["SHA"]
     if is_brawl("try"):
@@ -186,11 +203,15 @@ def create_test_jobs() -> list[TestMatrix]:
         )
 
     jobs.append(
-        TestMatrix(
-            os=LINUX_X86_64,
+        Job(
+            runner=LINUX_X86_64,
             job_name=f"Test (Linux x86_64)",
-            pr_number=pr_number(),
-            commit_sha=commit_sha,
+            job="test",
+            ffmpeg=True,
+            inputs=TestMatrix(
+                pr_number=pr_number(),
+                commit_sha=commit_sha,
+            ),
             rust=RustSetup(
                 toolchain="nightly",
                 components="llvm-tools-preview",
@@ -203,11 +224,15 @@ def create_test_jobs() -> list[TestMatrix]:
 
     if is_brawl():
         jobs.append(
-            TestMatrix(
-                os=LINUX_ARM64,
+            Job(
+                runner=LINUX_ARM64,
                 job_name=f"Test (Linux arm64)",
-                pr_number=pr_number(),
-                commit_sha=commit_sha,
+                job="test",
+                ffmpeg=True,
+                inputs=TestMatrix(
+                    pr_number=pr_number(),
+                    commit_sha=commit_sha,
+                ),
                 rust=RustSetup(
                     toolchain="nightly",
                     components="llvm-tools-preview",
@@ -221,15 +246,19 @@ def create_test_jobs() -> list[TestMatrix]:
     return jobs
 
 
-def create_grind_jobs() -> list[GrindMatrix]:
-    jobs: list[GrindMatrix] = []
+def create_grind_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     if is_brawl():
         jobs.append(
-            GrindMatrix(
-                os=LINUX_X86_64,
+            Job(
+                runner=LINUX_X86_64,
                 job_name=f"Grind (Linux x86_64)",
-                env="X86_64_UNKNOWN_LINUX_GNU=valgrind --error-exitcode=1 --leak-check=full --gen-suppressions=all --suppressions=$(pwd)/valgrind_suppressions.log",
+                job="grind",
+                ffmpeg=True,
+                inputs=GrindMatrix(
+                    env="X86_64_UNKNOWN_LINUX_GNU=valgrind --error-exitcode=1 --leak-check=full --gen-suppressions=all --suppressions=$(pwd)/valgrind_suppressions.log",
+                ),
                 rust=RustSetup(
                     toolchain="nightly",
                     shared_key="grind-linux-x86_64",
@@ -240,10 +269,14 @@ def create_grind_jobs() -> list[GrindMatrix]:
         )
 
         jobs.append(
-            GrindMatrix(
-                os=LINUX_ARM64,
+            Job(
+                runner=LINUX_ARM64,
                 job_name=f"Grind (Linux arm64)",
-                env="AARCH64_UNKNOWN_LINUX_GNU=valgrind --error-exitcode=1 --leak-check=full --gen-suppressions=all --suppressions=$(pwd)/valgrind_suppressions.log",
+                job="grind",
+                ffmpeg=True,
+                inputs=GrindMatrix(
+                    env="AARCH64_UNKNOWN_LINUX_GNU=valgrind --error-exitcode=1 --leak-check=full --gen-suppressions=all --suppressions=$(pwd)/valgrind_suppressions.log",
+                ),
                 rust=RustSetup(
                     toolchain="nightly",
                     shared_key="grind-linux-arm64",
@@ -255,13 +288,17 @@ def create_grind_jobs() -> list[GrindMatrix]:
 
     return jobs
 
-def create_fmt_jobs() -> list[FmtMatrix]:
-    jobs: list[FmtMatrix] = []
+
+def create_fmt_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     jobs.append(
-        FmtMatrix(
-            os=GITHUB_DEFAULT_RUNNER,
+        Job(
+            runner=GITHUB_DEFAULT_RUNNER,
             job_name=f"Fmt",
+            job="fmt",
+            ffmpeg=True,
+            inputs=FmtMatrix(),
             rust=RustSetup(
                 toolchain="nightly",
                 components="rustfmt",
@@ -273,13 +310,17 @@ def create_fmt_jobs() -> list[FmtMatrix]:
 
     return jobs
 
-def create_hakari_jobs() -> list[HakariMatrix]:
-    jobs: list[HakariMatrix] = []
+
+def create_hakari_jobs() -> list[Job]:
+    jobs: list[Job] = []
 
     jobs.append(
-        HakariMatrix(
-            os=GITHUB_DEFAULT_RUNNER,
+        Job(
+            runner=GITHUB_DEFAULT_RUNNER,
             job_name=f"Hakari",
+            job="hakari",
+            ffmpeg=True,
+            inputs=HakariMatrix(),
             rust=RustSetup(
                 toolchain="nightly",
                 components="rustfmt",
@@ -292,20 +333,23 @@ def create_hakari_jobs() -> list[HakariMatrix]:
 
     return jobs
 
+
 def create_jobs() -> list[Job]:
-    jobs: list[Job] = []
-    jobs.extend([Job(job_name="docs", inputs=job) for job in create_docs_jobs()])
-    jobs.extend([Job(job_name="clippy", inputs=job) for job in create_clippy_jobs()])
-    jobs.extend([Job(job_name="test", inputs=job) for job in create_test_jobs()])
-    jobs.extend([Job(job_name="grind", inputs=job) for job in create_grind_jobs()])
-    jobs.extend([Job(job_name="fmt", inputs=job) for job in create_fmt_jobs()])
-    jobs.extend([Job(job_name="hakari", inputs=job) for job in create_hakari_jobs()])
+    jobs = (
+        create_docs_jobs()
+        + create_clippy_jobs()
+        + create_test_jobs()
+        + create_grind_jobs()
+        + create_fmt_jobs()
+        + create_hakari_jobs()
+    )
 
     return jobs
 
 
 def main():
     print(f"matrix={json.dumps([asdict(job) for job in create_jobs()])}")
+
 
 if __name__ == "__main__":
     main()
