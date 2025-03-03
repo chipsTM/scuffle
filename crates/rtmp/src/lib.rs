f01c1694400fd9e98da1ca06580b02840a77a064
Lennart Kloock <lennart@scuffle.cloud>
2025-03-03 21:41:20 +0100

    fix(rtmp): add deny lints and root docs

--- crates/rtmp/src/lib.rs
+++ crates/rtmp/src/lib.rs
@@ -1,4 +1,20 @@
+//! A crate for handling RTMP server connections.
+//!
+//! ## Status
+//!
+//! This crate is currently under development and is not yet stable.
+//!
+//! Unit tests are not yet fully implemented. Use at your own risk.
+//!
+//! ## License
+//!
+//! This project is licensed under the [MIT](./LICENSE.MIT) or [Apache-2.0](./LICENSE.Apache-2.0) license.
+//! You can choose between one of them if you use this work.
+//!
+//! `SPDX-License-Identifier: MIT OR Apache-2.0`
 #![cfg_attr(all(coverage_nightly, test), feature(coverage_attribute))]
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
 
 mod chunk;
 mod handshake;
