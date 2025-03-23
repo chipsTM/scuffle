3a96cdbe1265a961a507b67b6eeb303187ec454a
Lennart Kloock <lennart@scuffle.cloud>
2025-03-23 22:28:50 +0100

    docs(rtmp): add doc example

--- crates/rtmp/src/lib.rs
+++ crates/rtmp/src/lib.rs
@@ -8,6 +8,51 @@
 //! | Enhancing RTMP, FLV | `v1-2024-02-29-r1` | <https://github.com/veovera/enhanced-rtmp/blob/main/docs/enhanced/enhanced-rtmp-v1.pdf> | |
 //! | Enhanced RTMP | `v2-2024-10-22-b1` | <https://github.com/veovera/enhanced-rtmp/blob/main/docs/enhanced/enhanced-rtmp-v2.pdf> | Refered to as 'Enhanced RTMP spec' in this documentation |
 //!
+//! ## Example
+//!
+//! ```no_run
+//! # use std::io::Cursor;
+//! #
+//! # use scuffle_rtmp::ServerSession;
+//! # use scuffle_rtmp::session::server::{ServerSessionError, SessionData, SessionHandler};
+//! # use tokio::net::TcpListener;
+//! #
+//! struct Handler;
+//!
+//! impl SessionHandler for Handler {
+//!     async fn on_data(&mut self, stream_id: u32, data: SessionData) -> Result<(), ServerSessionError> {
+//!         // Handle incoming video/audio/meta data
+//!         Ok(())
+//!     }
+//!
+//!     async fn on_publish(&mut self, stream_id: u32, app_name: &str, stream_name: &str) -> Result<(), ServerSessionError> {
+//!         // Handle the publish event
+//!         Ok(())
+//!     }
+//!
+//!     async fn on_unpublish(&mut self, stream_id: u32) -> Result<(), ServerSessionError> {
+//!         // Handle the unpublish event
+//!         Ok(())
+//!     }
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let listener = TcpListener::bind("[::]:1935").await.unwrap();
+//!     // listening on [::]:1935
+//!
+//!     while let Ok((stream, addr)) = listener.accept().await {
+//!         let session = ServerSession::new(stream, Handler);
+//!
+//!         tokio::spawn(async move {
+//!             if let Err(err) = session.run().await {
+//!                 // Handle the session error
+//!             }
+//!         });
+//!     }
+//! }
+//! ```
+//!
 //! ## Status
 //!
 //! This crate is currently under development and is not yet stable.
