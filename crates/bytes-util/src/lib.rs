b9969b43d77a7817ad5352e12aee5f8d7fbfbde0
Lennart Kloock <lennart@scuffle.cloud>
2025-04-02 21:58:41 +0200

    fix(bytes-util): docs

--- crates/bytes-util/src/lib.rs
+++ crates/bytes-util/src/lib.rs
@@ -6,6 +6,7 @@
 //! You can choose between one of them if you use this work.
 //!
 //! `SPDX-License-Identifier: MIT OR Apache-2.0`
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(all(coverage_nightly, test), feature(coverage_attribute))]
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
